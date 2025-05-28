12d841fb7130bcd0d43f039ee0c5ecf47844c068
AdrijaInGithub <adrija3002biswas@gmail.com>
2025-05-28 11:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const loginLink=document.querySelector('.login-link');
 const registerLink=document.querySelector('.register-link');
 const btnPopup=document.querySelector('.btnLogin-popup');
 const iconClose=document.querySelector('.icon-close');
+let Logeyeicon=document.getElementById("login-eyeicon");
+let Logpassword=document.getElementById("login-password");
+let Regeyeicon=document.getElementById("register-eyeicon");
+let Regpassword=document.getElementById("register-password");
 
 registerLink.addEventListener('click',()=>{
   wrapper.classList.add('active');
@@ -19,3 +23,21 @@ btnPopup.addEventListener('click',()=>{
 iconClose.addEventListener('click',()=>{
   wrapper.classList.remove('active-popup');
 });
+
+Logeyeicon.onclick=function(){
+  if(Logpassword.type=="password"){
+    Logpassword.type="text";
+  }
+  else{
+    Logpassword.type="password";
+  }
+}
+
+Regeyeicon.onclick=function(){
+  if(Regpassword.type=="password"){
+    Regpassword.type="text";
+  }
+  else{
+    Regpassword.type="password";
+  }
+}
