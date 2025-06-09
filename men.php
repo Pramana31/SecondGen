d5dc262522123ae21523da6fb47fa1a7fa9c1ca5
AdrijaInGithub <adrija3002biswas@gmail.com>
2025-06-09 11:10:44 +0530

    Add files via upload

---
+++ men.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SecondGen</title>
+    <link rel="icon" href="logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style_women.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <section>
+        <div>
+            <div class="shop-section">
+        <div class="box" >
+            <div class="box-content">
+                <div class="box-img" style="background-image: url('menwear.jpg');"></div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="box-content">
+                <div class="box-img" style="background-image: url('menwear1.jpg');">
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="box-content">
+                <div class="box-img" style="background-image: url('jeans1.jpg');">
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="box-content">
+                <div class="box-img" style="background-image: url('menwear2.jpg');">
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="box-content">
+                <div class="box-img" style="background-image: url('menwear3.jpg');">
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="box-content">
+                <div class="box-img" style="background-image: url('men_jacket.jpg');">
+                </div>
+            </div>
+        </div>
+    </div>
+        </div>
+    </section>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
