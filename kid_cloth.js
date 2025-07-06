efcddfb9538c8a136126987617bb9a23d8cc0739
AdrijaInGithub <adrija3002biswas@gmail.com>
2025-07-06 22:38:50 +0530

    Add files via upload

--- kid_cloth.js
+++ kid_cloth.js
@@ -1,73 +1,59 @@
   const cloth= [
   {
-    "id": 1 ,
-    "name": "Jacket",
-    "price": 300,
-    "description" : "M-size,Off-White Denim Crop Jacket",
+    "id": 'kid_1' ,
+    "name": "Pink Sharara",
+    "price": 200,
+    "description" : "Floral Printed Top-Piece ,Pink Sharara Pant and Pink Dupatta",
     "image": "kidwear2.jpg"
   },
   {
-    "id": 2 ,
-    "name": "Formal Suit",
-    "price": 800,
-    "description" : "S-size,170cm(or 5.6ft)Beige Formal Suit Set 100% cotton",
+    "id": 'kid_2' ,
+    "name": "Blue Princess Gown",
+    "price": 500,
+    "description" : "Gorgeous Blue Ombre Effect Gown with a blue Ribbon Bow",
     "image": "kidwear3.jpg"
   },
   {
-  "id": 3 ,
-  "name": "Top",
-  "price": 350,
-  "description" : "XXL-size,Olive Green Top 100% cotton",
+  "id": 'kid_3' ,
+  "name": "Maroon Panjabi",
+  "price": 250,
+  "description" : "Traditional Maroon Panjabi (3 year old child)",
   "image": "kidwear8.jpg"
   },
   {
-  "id": 4 ,
-  "name": "Blue Casual Dress",
-  "price": 300,
-  "description" : "M-size,Navi Blue Block Printed Cotton Dress 100%cotton",
+  "id": 'kid_4' ,
+  "name": "Pink  Dress",
+  "price": 200,
+  "description" : "Traditional Pink Kurti with Frilled Sleeves 100%cotton",
   "image": "kidwear5.jpg"
   },
   {
-  "id": 5 ,
-  "name": "Salwar Suit",
-  "price": 500,
-  "description" : "L-size,160cm(or 5.3ft) White Salwar Suit covered with Indian Motif print 100% cotton",
+  "id": 'kid_5' ,
+  "name": "Kurta & Sticted Dhoti",
+  "price": 499,
+  "description" : "Blue Panjabi Dhoti with block prints 100%cotton",
   "image": "kidwear6.jpg"
   },
   {
-  "id": 6 ,
-  "name": "Wide Jeans",
-  "price": 450,
-  "description" : "M-size,175cm(or 5.8ft) High Waist Light Blue Denim Jeans",
+  "id": 'kid_6' ,
+  "name": "White Frock",
+  "price": 350,
+  "description" : "Frilled Princess Frock for Party Wear",
   "image": "kidwear7.jpg"
   },
   {
-  "id": 7 ,
-  "name": "Green Casual Dress",
-  "price": 475,
-  "description" : "S-size,White Dress imprinted with bright Greenish-yellow print",
-  "image": "suit3.jpg"
+  "id": 'kid_7' ,
+  "name": "Grey Sherwani",
+  "price": 275,
+  "description" : "Sequins worked Silver Shrewani for Party/Traditional Purpous",
+  "image": "kidwear10.jpg"
   },
   {
-  "id": 8 ,
-  "name": "Yellow Lehanga",
-  "price": 699,
-  "description" : "L-size,160cm(or 5.3ft),Golden Yellow Lightly Emboidered Lehenga",
-  "image": "lehenga.jpg"
-  },
-  {
-  "id": 9 ,
-  "name": "Ankle Jeans",
-  "price": 400,
-  "description" : "28cm waist,Greyish Ankle/Cropped Denim Jeans",
-  "image": "jeans1.jpg"
-  },
-  {
-  "id": 10 ,
-  "name": "Red Party Dress",
-  "price": 550,
-  "description" : "M-size,Red Satin Strapped Dress",
-  "image": "dress2.jpg"
+  "id": 'kid_8' ,
+  "name": "Multicolored Lehanga",
+  "price": 299,
+  "description" : "Multicolour Printed Traditional Lehenga 100%cotton",
+  "image": "kidwear9.jpg"
   }
 ]
 export default cloth;
